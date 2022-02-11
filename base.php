1ec0ba2cda8af3f3b0593ad049b819a3900bc9e6
rong <rongyy94job@gmail.com>
2022-02-11 13:53:12 +0800

    順便建置管理登入功能

--- base.php
+++ base.php
@@ -1,168 +1,61 @@
 <?php
-date_default_timezone_set("Asia/Taipei");
 session_start();
+date_default_timezone_set('Asia/Taipei');
 
-class DB
-{
-
-  
-    private $dsn = "mysql:host=localhost;charset=utf8;dbname=web01_try4";
-    // private $root = 'root';
-    // private $password = '';
-    private $table;
+class DB{
+    private $dsn="mysql:host=localhost;charset=utf8;dbname=web04";
     private $pdo;
-
-   
-    public function __construct($table)
-    {
-       
-        $this->table = $table;
+    public $table;
+    
+    function __construct($table){
+        $this->table=$table;
         // $this->pdo = new PDO($this->dsn, $this->root, $this->password);
-        $this-> pdo = new PDO($this->dsn,'root','');
+        $this->pdo=new PDO($this->dsn,'root','');
+        
     }
 
-    
-    public function all(...$arg)
-    {
-        
-
-        $sql = "SELECT * FROM $this->table ";
-
-
-        
-        switch (count($arg)) {
+    function all(...$arg){
+        $sql="SELECT * FROM $this->table ";
+        switch(count($arg)){
             case 1:
-
-             
-                if (is_array($arg[0])) {
-
-                
-                    foreach ($arg[0] as $key => $value) {
-
-                        $tmp[] = "`$key`='$value'";
+                if(is_array($arg[0])){
+                    foreach($arg[0] as $key => $val){
+                        $tmp[]="`$key`='$val'";
                     }
-
-                   
-                    $sql .= " WHERE " . implode(" AND ", $tmp);
-                } else {
-
-                
+                    $sql .= " WHERE " . implode(" && ",$tmp);
+                }else{
                     $sql .= $arg[0];
                 }
-                break;
+            break;
             case 2:
-
-              
-                foreach ($arg[0] as $key => $value) {
-
-                    $tmp[] = "`$key`='$value'";
+                foreach($arg[0] as $key => $val){
+                    $tmp[]="`$key`='$val'";
                 }
+                $sql .= " WHERE " . implode(" && ",$tmp);
+                $sql .= $arg[1];
 
-              
-                $sql .= " WHERE " . implode(" AND ", $tmp) . " " . $arg[1];
-                break;
-
-              
+            break;
+        
         }
-
 
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
-   
-    public function math($math, $col, ...$arg)
-    {
-        $sql = "SELECT $math($col) FROM $this->table ";
+    function find($id){
+        $sql="SELECT * FROM $this->table ";
 
- 
-        switch (count($arg)) {
-            case 1:
-
-           
-                if (is_array($arg[0])) {
-
-                    
-                    foreach ($arg[0] as $key => $value) {
-
-                        $tmp[] = "`$key`='$value'";
-                    }
-
-                  
-                    $sql .= " WHERE " . implode(" AND ", $tmp);
-                } else {
-
-                   
-                }
-                break;
-            case 2:
-
-             
-                foreach ($arg[0] as $key => $value) {
-
-                    $tmp[] = "`$key`='$value'";
-                }
-
-               
-                $sql .= " WHERE " . implode(" AND ", $tmp) . " " . $arg[1];
-                break;
-
-   
-        }
-
-
-     
-        return $this->pdo->query($sql)->fetchColumn();
-    }
-
-   
-    public function find($id)
-    {
-       
-        $sql = "SELECT * FROM $this->table WHERE ";
-
-        if (is_array($id)) {
-
-            foreach ($id as $key => $value) {
-
-                $tmp[] = "`$key`='$value'";
+        if(is_array($id)){
+            foreach($arg[0] as $key => $val){
+                $tmp[]="`$key`='$val'";
             }
-
-            $sql .= implode(" AND ", $tmp);
-        } else {
-
-            $sql .= " `id`='$id'";
+            $sql .= " WHERE " . implode(" && ",$tmp);
+        }else{
+            $sql .= " WHERE `id`='$id'";
         }
-
 
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
-
-   
-    public function del($id)
-    {
-        
-        $sql = "DELETE FROM $this->table WHERE ";
-
-        if (is_array($id)) {
-
-            foreach ($id as $key => $value) {
-
-                $tmp[] = "`$key`='$value'";
-            }
-
-            $sql .= implode(" && ", $tmp);
-        } else {
-
-            $sql .= "`id`='$id'";
-        }
-
-        
-        return $this->pdo->exec($sql);
-    }
-
-    public function save($array)
-    {
-
+    function save($array){
         if(isset($array['id'])){
             foreach($array as $key => $val){
                 $tmp[]="`$key`='$val'";
@@ -180,29 +73,62 @@ class DB
         return $this->pdo->exec($sql);
 
     }
+    function del($id){
+        $sql="DELETE FROM $this->table ";
 
-   
-    public function q($sql)
-    {
-       
-        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        if(is_array($id)){
+            foreach($arg[0] as $key => $val){
+                $tmp[]="`$key`='$val'";
+            }
+            $sql .= " WHERE " . implode(" && ",$tmp);
+        }else{
+            $sql .= " WHERE `id`='$id'";
+        }
+
+        return $this->pdo->exec($sql);
     }
+    function q($sql){
+        return $this->$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    function math($math,$col,...$arg){
+        $sql="SELECT $math($col) FROM $this->table ";
+        switch(count($arg)){
+            case 1:
+                if(is_array($arg[0])){
+                    foreach($arg[0] as $key => $val){
+                        $tmp[]="`$key`='$val'";
+                    }
+                    $sql .= " WHERE " . implode(" && ",$tmp);
+                }else{
+                    $sql .= $arg[0];
+                }
+            break;
+            case 2:
+                foreach($arg[0] as $key => $val){
+                    $tmp[]="`$key`='$val'";
+                }
+                $sql .= " WHERE " . implode(" && ",$tmp);
+                $sql .= $arg[1];
+
+            break;
+        
+        }
+        //echo $sql;
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
 }
 
-
-function to($url)
-{
-   
+function to($url){
+    header("location:".$url);
 }
 
 function dd($array){
     echo "<pre>";
     print_r($array);
-    echo "<pre>";
-
+    echo "</pre>";
 }
 
+$Mem=new DB("member");
 
-$User = new DB('user');
-$Menu = new DB('menu');
-//etc......
+?>
--- front/reg.php
+++ front/reg.php
@@ -1,3 +1,5 @@
+<!-- //正式的話帳號不可以為空白，這邊題目不用檢查所以沒做，但實務上記得不能這樣
+     題組2有檢查 -->
 <!-- 簡寫 h1.ct -->
 <h1 class="ct">會員註冊</h1>
 <!-- 簡寫 table.all>tr*6>td.tt.ct+td.pp>input:text -->
@@ -31,7 +33,42 @@
     </tr>
 </table>
 <div class="ct">
-<button onclick="reg()">註冊</button>
-<button onclick="reset()">重置</button>
-
+    <button onclick="reg()">註冊</button>
+    <button onclick="reset()">重置</button>
 </div>
+<!-- form api -->
+<script>
+    function chkAcc(){
+        let acc=$("#acc").val()
+        $.post("api/chk_acc.php",{acc},(chk)=>{
+        // unbox解包 js在判斷數字的情況下，有時候會解包成功以時候會失敗
+        //比如說字串的0>1 有時會ture有時候faluse
+        //有時chk會被當成字串，加parselnt轉成數字(確定格式是我們要的比較保險)
+        if(parseInt(chk) || acc=='admin'){
+            alert("帳號已存在")
+        }else{
+            alert("此帳號可使用")
+        }
+    })
+}
+function reg(){
+    let data={
+        acc:$("#acc").val(),
+        name:$("#name").val(),
+        pw:$("#pw").val(),
+        addr:$("#addr").val(),
+        tel:$("#tel").val(),
+        email:$("#email").val(),
+    }
+    $.post("api/chk_acc.php",{acc:data.acc},(chk)=>{
+        if(parseInt(chk) || data.acc=='admin'){
+            alert("帳號已存在")
+        }else{
+            $.post("api/reg.php",data,()=>{
+                alert("註冊完成，歡迎加入")
+                location.href='?do=login'
+            })
+        }
+    })
+}
+</script>
