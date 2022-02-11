335096828aef98d6e6fd908941a6448d33b1f4fa
rong <rongyy94job@gmail.com>
2022-02-11 16:33:51 +0800

    改用form來完成管理者功能及刪除資料ajax功能

--- api/save_admin.php
+++ api/save_admin.php
@@ -2,3 +2,4 @@
 
 $_POST['pr']=serialize($_POST['pr']);
 $Admin->save($_POST);
+to('../back.php?do=admin');
--- back/add_admin.php
+++ back/add_admin.php
@@ -1,30 +1,32 @@
 <h1 class="ct">新增管理帳號</h1>
-<table class="all">
-    <tr>
-        <td class="tt ct">帳號</td>
-        <td class="pp"><input type="text" name="acc" id="acc"></td>
-    </tr>
-    <tr>
-        <td class="tt ct">密碼</td>
-        <td class="pp"><input type="password" name="pw" id="pw"></td>
-    </tr>
-    <tr>
-        <td class="tt ct">權限</td>
-        <td class="pp">
-            <div><input type="checkbox" name="pr[]" value="1">商品分類與管理</div>
-            <div><input type="checkbox" name="pr[]" value="2">訂單管理</div>
-            <div><input type="checkbox" name="pr[]" value="3">會員管理</div>
-            <div><input type="checkbox" name="pr[]" value="4">頁尾版權區管理</div>
-            <div><input type="checkbox" name="pr[]" value="5">最新消息管理</div>
-        </td>
-    </tr>
-</table>
-<div class="ct">
-    <button onclick="addAdmin()">新增</button>
-    <button onclick="reset()">重置</button>
-</div>
+<form action="api/save_admin.php" method="post">
+    <table class="all">
+        <tr>
+            <td class="tt ct">帳號</td>
+            <td class="pp"><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">密碼</td>
+            <td class="pp"><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">權限</td>
+            <td class="pp">
+                <div><input type="checkbox" name="pr[]" value="1">商品分類與管理</div>
+                <div><input type="checkbox" name="pr[]" value="2">訂單管理</div>
+                <div><input type="checkbox" name="pr[]" value="3">會員管理</div>
+                <div><input type="checkbox" name="pr[]" value="4">頁尾版權區管理</div>
+                <div><input type="checkbox" name="pr[]" value="5">最新消息管理</div>
+            </td>
+        </tr>
+    </table>
+    <div class="ct">
+        <button type="submit">新增</button> | 
+        <button type="reset">重置</button>
+    </div>
+</form>
 <script>
-function reset(){
+/* function reset(){
     $("#acc,#pw").val("")
     $("input[type='checkbox']").prop('checked',false)
 }
@@ -40,5 +42,5 @@ $.post("api/save_admin.php",
         ()=>{
            location.href="?do=admin" 
         })
-}
+} */
 </script>
