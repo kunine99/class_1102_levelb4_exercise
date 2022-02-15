effbfd4e28c94dc42077f760ec95e7112d64fde8
rong <rongyy94job@gmail.com>
2022-02-15 11:35:25 +0800

    簡化新增分類的js函式為一種

--- api/new_big.php
+++ api/new_big.php
@@ -1,8 +1,9 @@
 <?php include_once "../base.php";
+//這檔案本來在api/new_big
 // 假設忘記設預設值，先在這邊設為0
 // 我送過來big的值，但因為欄位不一樣所以資料庫毫無反應
-$_POST['parent']=0;
-$Type->save($_POST);
+// $_POST['parent']=0;
+// $Type->save($_POST);
 
 
 
--- back/th.php
+++ back/th.php
@@ -2,12 +2,12 @@
 <h1 class="ct">商品分類</h1>
 <div class="ct">新增大分類
     <input type="text" name="big" id="big">
-    <button onclick='newBig()'>新增</button>
+    <button onclick="newType('big')">新增</button>
 </div>
 <div class="ct">新增中分類
-    <select name="parent" id="parent"></select> 
+    <select name="parent" id="parent"></select>
     <input type="text" name="mid" id="mid">
-    <button onclick='newMid()'>新增</button>
+    <button onclick="newType('mid')">新增</button>
 </div>
 <!-- 分類區 -->
 <hr>
@@ -15,11 +15,11 @@
 <div class="ct"><button>新增商品</button></div>
 <table class="all">
     <tr class="tt ct">
-    <td>編號</td>
-    <td>商品名稱</td>
-    <td>庫存量</td>
-    <td>狀態</td>
-    <td>操作</td>
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>庫存量</td>
+        <td>狀態</td>
+        <td>操作</td>
     </tr>
     <tr class="pp ct">
         <td></td>
@@ -35,21 +35,47 @@
     </tr>
 </table>
 <script>
-// 請在我parent的項目 用load
-//我一載入這個畫面他就會幫我執行jquery的程式
-//沒有用function包起來的程式會直接執行
-$("#parent").load("api/get_big.php")
+    // 請在我parent的項目 用load
+    //我一載入這個畫面他就會幫我執行jquery的程式
+    //沒有用function包起來的程式會直接執行
+    $("#parent").load("api/get_big.php")
 
-    function newBig(){
-        // 把送出來的值name換成big=name用big這個值
-        // let Big=$("#big").val();
-        $.post("api/new_big.php",{name:$("#big").val()},(res)=>{
-            // console.log(res);
-            //會造成畫面重整
-            location.reload();  
+    function newType(type) {
+        let name, parent
+        switch (type) {
+            case "big":
+                name = $("#big").val();
+                parent = 0;
+                break;
+            case 'mid':
+                name = $("#mid").val();
+                parent = $("#parent").val();
+                break;
+        }
+        $.post("api/save_type.php", {
+            name,
+            parent
+        }, (res) => {
+            location.reload();
         })
     }
+
+
+
+    /* function newBig(){
+    // 把送出來的值name換成big=name用big這個值
+   // let big=$("#big").val();
+    $.post("api/save_type.php",{name:$("#big").val(),parent:0},(res)=>{
+             // console.log(res);
+            //會造成畫面重整
+        location.reload();
+    })
+}
+function newMid(){
+   let parent=$("#parent").val();
+   let name=$("#mid").val();
+    $.post("api/save_type.php",{name,parent},(res)=>{
+        location.reload();
+    })
+} */
 </script>
-
-
-
