42e8d097741902acc9b3929bd340b2baa10eaca3
rong <rongyy94job@gmail.com>
2022-02-15 14:29:02 +0800

    顯示分類列表

--- back/th.php
+++ back/th.php
@@ -1,15 +1,50 @@
 <!-- div.ct*2>input:text -->
 <h1 class="ct">商品分類</h1>
-<div class="ct">新增大分類
-    <input type="text" name="big" id="big">
+<div class='ct'>新增大分類
+    <input type="text" name='big' id="big">
     <button onclick="newType('big')">新增</button>
 </div>
-<div class="ct">新增中分類
+<div class='ct'>新增中分類
     <select name="parent" id="parent"></select>
     <input type="text" name="mid" id="mid">
     <button onclick="newType('mid')">新增</button>
 </div>
-<!-- 分類區 -->
+<!--分類區-->
+<!-- table.all>tr.tt>td+td.ct>button*2 -->
+<table class="all">
+    <?php
+    $bigs=$Type->all(['parent'=>0]);
+    foreach($bigs as $big){
+    ?>
+    <tr class="tt">
+        <td><?=$big['name'];?></td>
+        <td class="ct">
+            <button onclick="edit(<?=$big['id'];?>)">修改</button>
+            <button onclick="del('type',<?=$big['id'];?>)">刪除</button>
+            <!-- 
+            min-height跟max-height差在上下線
+            max有東西就有高度，沒有東西就沒有高度
+            mix是不管怎樣都有高度...?
+            -->
+        </td>
+    </tr>
+    <?php
+        $mids=$Type->all(['parent'=>$big['id']]);
+        if(count($mids)>0){
+            foreach($mids as $mid){
+    ?>
+    <tr class="pp ct">
+        <td><?=$mid['name'];?></td>
+        <td>
+            <button onclick="edit(<?=$mid['id'];?>)">修改</button>
+            <button onclick="del('type',<?=$mid['id'];?>)">刪除</button>
+        </td>
+    </tr>
+    <?php }  }  }  ?>
+</table>
+
+
+
 <hr>
 <h1 class="ct">商品管理</h1>
 <div class="ct"><button>新增商品</button></div>
@@ -35,42 +70,26 @@
     </tr>
 </table>
 <script>
-    // 請在我parent的項目 用load
-    //當我在瀏覽器按出送出的時候，網址列打完後
-    // 1.我的網路到對方的service端
-    // 2.頁面回傳到client端
-    //我一載入這個畫面他就會幫我執行jquery的程式
-    //沒有用function包起來的程式會直接執行
-    $("#parent").load("api/get_big.php");
-
-    function newType(type) {
-        let name, parent
-        switch (type) {
-            case "big":
-                name = $("#big").val();
-                parent = 0;
-                break;
-            case 'mid':
-                name = $("#mid").val();
-                parent = $("#parent").val();
-                break;
-        }
-        $.post("api/save_type.php", {
-            name,
-            parent
-        }, (res) => {
-            location.reload();
-        })
+$("#parent").load("api/get_big.php")
+function newType(type){
+    let name,parent
+    switch(type){
+        case "big":
+            name=$("#big").val();
+            parent=0;
+        break;
+        case 'mid':
+            name=$("#mid").val();
+            parent=$("#parent").val();
+        break;
     }
-
-
-
-    /* function newBig(){
-    // 把送出來的值name換成big=name用big這個值
+    $.post("api/save_type.php",{name,parent},(res)=>{
+        location.reload();
+    })
+}
+/* function newBig(){
    // let big=$("#big").val();
     $.post("api/save_type.php",{name:$("#big").val(),parent:0},(res)=>{
-             // console.log(res);
-            //會造成畫面重整
         location.reload();
     })
 }
