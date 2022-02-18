9de7844bbf45a75c83060e7866fcfee08a8e2a07
rong <rongyy94job@gmail.com>
2022-02-18 15:33:23 +0800

    建置不同分類下的商品變數

--- front/main.php
+++ front/main.php
@@ -2,19 +2,28 @@
 $type=$_GET['type']??0;
 if($type==0){
     $nav="全部商品";
+    $rows=$Goods->all(['sh'=>1]);
 }else{
     $t=$Type->find($type);
     if($t['parent']==0){
         $nav=$t['name'];
+        $rows=$Goods->all(['sh'=>1,'big'=>$type]);
     }else{
         $b=$Type->find($t['parent']);
         $nav= $b['name'] ." > ".$t['name'];
+        $rows=$Goods->all(['sh'=>1,'mid'=>$type]);
     }
+}
+
+?>
+
+<h1><?=$nav;?></h1>
+
+<?php
+foreach($rows as $row){
+    echo $row['name'];
+    echo "<br>";
 }
 
 
 ?>
-
-
-
-<h1><?=$nav;?></h1>
