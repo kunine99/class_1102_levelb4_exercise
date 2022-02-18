e3ff62e736cdb2d674c82c5379fec105022cc2b7
rong <rongyy94job@gmail.com>
2022-02-18 11:37:44 +0800

    完成修改商品功能

--- back/edit_goods.php
+++ back/edit_goods.php
@@ -16,30 +16,30 @@
         </tr>
         <tr>
             <td class="tt ct">商品編號</td>
-            <td class="pp">完成分類後自動分配</td>
+            <td class="pp"><?=$g['no'];?></td>
         </tr>
         <tr>
             <td class="tt ct">商品名稱</td>
             <td class="pp">
-            <input type="text" name="name" id="name">
-            </td>
+            <input type="text" name="name" value="<?=$g['name'];?>">
+        </td>
         </tr>
         <tr>
             <td class="tt ct">商品價格</td>
             <td class="pp">
-            <input type="text" name="price" id="price">
+            <input type="text" name="price" value="<?=$g['price'];?>">
             </td>
         </tr>
         <tr>
             <td class="tt ct">規格</td>
             <td class="pp">
-            <input type="text" name="spec" id="spec">
+            <input type="text" name="spec" value="<?=$g['spec'];?>">
             </td>
         </tr>
         <tr>
             <td class="tt ct">庫存量</td>
             <td class="pp">
-            <input type="text" name="stock" id="stoc">
+            <input type="text" name="stock" value="<?=$g['stock'];?>">
             </td>
         </tr>
         <tr>
@@ -51,19 +51,27 @@
         <tr>
             <td class="tt ct">商品介紹</td>
             <td class="pp">
-            <textarea name="intro" id="intro" style="width:90%;height:100px"></textarea>
+            <textarea name="intro" id="intro" style="width:90%;height:100px"><?=$g['intro'];?></textarea>
             </td>
         </tr>
     </table>
     <div class="ct">
-        <button type="submit">新增</button> | 
+    <input type="hidden" name="id" value="<?=$g['id'];?>">
+        <button type="submit">修改</button> | 
         <button type="reset">重置</button> | 
         <button type="button" onclick="location.href='?do=th'">返回</button>
     </div>
 </form>
 <script>
 $("#big").load('api/get_type.php',()=>{
-    $("#mid").load('api/get_type.php',{parent:$("#big").val()})
+   
+    //大分類的選項們載入完成時,去選定商品的大分類
+    $("#big option[value='<?=$g['big'];?>']").prop('selected',true);
+
+    $("#mid").load('api/get_type.php',{parent:$("#big").val()},()=>{
+        //中分類的選項們載入完成時,去選定商品的中分類
+        $("#mid option[value='<?=$g['mid'];?>']").prop('selected',true);
+    })
 })
 $("#big").on("change",function(){
     $("#mid").load("api/get_type.php",{parent:$("#big").val()})
