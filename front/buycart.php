6270827d53138ffab5d26e58097f99a38135e6dc
rong <rongyy94job@gmail.com>
2022-02-22 13:07:03 +0800

    建立購物車機制

--- front/buycart.php
+++ front/buycart.php
@@ -1,10 +1,16 @@
 <?php 
+if(isset($_GET["id"]) && isset($_GET['qt'])){
+    $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+}
 
 if(!isset($_SESSION['mem'])){
     to("?do=login");
     exit();
 }
 
-echo "你要購買的商品id 為".$_GET['id'];
-echo "<hr>";
-echo "數量為".$_GET['qt'];
+if(empty($_SESSION['cart'])){
+    echo "<h1 class='ct'>購物車中無商品</h1>";
+
+}else{
+   print_r($_SESSION['cart']);
+}
