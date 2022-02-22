ebdb4909ee2746e03d5a1f02e267bd2997a0075f
rong <rongyy94job@gmail.com>
2022-02-22 13:08:14 +0800

    列出購物車內容

--- front/buycart.php
+++ front/buycart.php
@@ -12,5 +12,40 @@ if(empty($_SESSION['cart'])){
     echo "<h1 class='ct'>購物車中無商品</h1>";
 
 }else{
-   print_r($_SESSION['cart']);
+    ?>
+    <h1 class="ct"><?=$_SESSION['mem'];?>的購物車</h1>
+    <table class="all">
+        <tr class="tt ct">
+            <td>編號</td>
+            <td>商品名稱</td>
+            <td>數量</td>
+            <td>庫存量</td>
+            <td>單價</td>
+            <td>小計</td>
+            <td>刪除</td>
+        </tr>
+        <?php 
+        foreach($_SESSION['cart'] as $id => $qt){
+            $item=$Goods->find($id);
+        ?>
+        <tr class="pp ct">
+            <td><?=$item['no'];?></td>
+            <td><?=$item['name'];?></td>
+            <td><?=$qt;?></td>
+            <td><?=$item['stock'];?></td>
+            <td><?=$item['price'];?></td>
+            <td><?=$item['price']*$qt;?></td>
+            <td>
+                <img src="icon/0415.jpg" >
+            </td>
+        </tr>
+        <?php 
+        }
+        ?>
+    </table>
+    <div class="ct">
+        <img src="icon/0411.jpg" onclick="location.href='index.php'">&nbsp;&nbsp;
+        <img src="icon/0412.jpg" onclick="location.href='?do=checkout'">
+    </div>
+    <?php
 }
